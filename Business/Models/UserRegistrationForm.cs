ad24e42fd8496b742168932df5e02c2a0523d4d3
Tim <tim..newton.larsson@outlook.com>
2024-12-22 20:29:45 +0100

    Starting with the User model och User service. Creating a simple list to begin with

---
+++ Business/Models/UserRegistrationForm.cs
@@ -0,0 +1,10 @@
+﻿using System.Dynamic;
+
+namespace Business.Models;
+
+public class UserRegistrationForm
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Id { get; set; } = null!;
+}
---
+++ Business/Services/ContactService.cs
@@ -0,0 +1,17 @@
+﻿using Business.Models;
+namespace Business.Services;
+
+public class ContactService
+{
+    private readonly List<UserRegistrationForm> _user = [];
+
+    public void AddContact(UserRegistrationForm user)
+    {
+        _user.Add(user);
+    }
+
+    public IEnumerable<UserRegistrationForm> ViewContacts()
+    {
+        return _user;
+    }
+}
