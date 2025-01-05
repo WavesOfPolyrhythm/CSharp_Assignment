a08e6949d4dc77090821b83fc63414a6cee92acd
Tim <tim..newton.larsson@outlook.com>
2025-01-05 21:17:41 +0100

    Include a test to check if IdentifierGenerator creates a unique Id of type GUID

---
+++ Business_Tests/Helpers/IdentifierGenerator_Tests.cs
@@ -0,0 +1,27 @@
+﻿using Business.Helpers;
+
+namespace Business_Tests.Helpers;
+
+public class IdentifierGenerator_Tests
+{
+    [Fact]
+
+    public void GenerateUniqueId_ShouldReturnStringOfTypeGuid()
+    {
+        //Test verifies that the generated ID is unique nad type of GUID
+
+
+        //ACT
+        string id1 = IdentifierGenerator.GenerateUniqueId();
+        string id2 = IdentifierGenerator.GenerateUniqueId();
+
+        //ASSERT 
+        Assert.False(string.IsNullOrEmpty(id1));
+
+        //The generated ID should be a valid GUID
+        Assert.True(Guid.TryParse(id1, out _));
+
+        //Verfies that both Id´s are unique
+        Assert.NotEqual(id1, id2);
+    }
+}
