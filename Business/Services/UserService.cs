2d2ff54de55c7397b6bccd2a6758e8d898d1d64f
Tim <tim..newton.larsson@outlook.com>
2025-01-07 21:51:24 +0100

    Added Try and Catch to UserService

--- Business/Services/UserService.cs
+++ Business/Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Entities;
 using Business.Models;
+using System.Diagnostics;
 namespace Business.Services;
 
 public class UserService
@@ -9,15 +10,39 @@ public class UserService
 
 
     // Adds the new UserEntity to the in-memory list and saves the updated list to the file.
-    public void CreateContact(UserEntity user)
+    public bool CreateContact(UserEntity user)
     {
-        _users.Add(user);
-        _fileService.SaveListToFile(_users);
+        try
+        {
+            //Add user to the list
+            _users.Add(user);
+
+            //Save the updated list to file
+            _fileService.SaveListToFile(_users);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            //Handls exception
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
     }
 
     public IEnumerable<UserEntity> ViewContacts()
     {
-        _users = _fileService.LoadListFromFile();
+        try
+        {
+            //Load list from file
+            _users = _fileService.LoadListFromFile();
+        }
+        catch (Exception ex)
+        {
+            //Handls exception
+            Debug.WriteLine(ex.Message);
+
+            _users = [];
+        }
         return _users;
     }
 }
